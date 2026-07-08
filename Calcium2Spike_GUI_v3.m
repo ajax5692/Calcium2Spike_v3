25d4180a641d91702e67d40ad577f414ab57a0b1
ajax5692 <nn.abhra@gmail.com>
2026-07-08 12:58:18 +0200

    feat mod: GUI updates
    Introduction of space cards for separate GUI components

--- Calcium2Spike_GUI_v3.m
+++ Calcium2Spike_GUI_v3.m
@@ -54,12 +54,16 @@ UISet_C2S(loggingConsole, 'HorizontalAlignment','Left');
 data.GUI.loggingConsole = loggingConsole;
 guidata(figureCalcium2Spike_GUI, data);
 
-%%% - File Selection Console"
+%%% - 1. Project management Console"
 ExperimentFilesUIGroup = uipanel('Title','Project management',...
-    'Position',[0.025, 0.42, 0.48, 0.4],'BackgroundColor','White');
+    'FontSize', 18,...
+    'Position',[0.025, 0.38, 0.48, 0.45],...
+    'ForegroundColor',[0.5, 0.5, 0.5],...
+    'BackgroundColor','White');
 
-% - Save location spacecard
+% - 1a. Save location spacecard
 SaveLocationUIGroup = uipanel('Title','Save location',...
+    'FontSize', 15, ...
     'Position',[0.030505952380952,0.673280423280423,0.468749999999999,0.11375661375661],'BackgroundColor',[0.75 0.75 0.75]);
 
 generalTB(1) = GenericTextBox(SaveLocationUIGroup);
@@ -90,12 +94,13 @@ UISet_C2S(PB(2), 'HorizontalAlignment', 'left');
 UISet_C2S(PB(2), 'Position', [0.881646655231561,0.057971014492756,0.110291595197254,0.260869565217391]);
 
 
-% - MESc file selection spacecard
+% - 1b. MESc file selection spacecard
 MEScUIGroup = uipanel('Title','MESc file',...
-    'Position',[0.030505952380952,0.442063492063488,0.468749999999999,0.21375661375661],'BackgroundColor',[0.75 0.75 0.75]);
+    'FontSize', 15, ...
+    'Position',[0.030505952380952,0.402063492063488,0.468749999999999,0.255375661375661],'BackgroundColor',[0.75 0.75 0.75]);
 
 generalTB(3) = GenericTextBox(MEScUIGroup);
-UISet_C2S(generalTB(3), 'Position', [0.01,0.6,0.974271012006861,0.289855072463767]);
+UISet_C2S(generalTB(3), 'Position', [0.01,0.63,0.974271012006861,0.289855072463767]);
 UISet_C2S(generalTB(3), 'String',char(hex2dec('2713')));
 UISet_C2S(generalTB(3), 'HorizontalAlignment', 'left');
 UISet_C2S(generalTB(3), 'ForeGroundColor', [0.4 0.4 0.4]);
@@ -111,58 +116,130 @@ UISet_C2S(PB(3), 'Callback', @callback_SelectMEScFile_C2S);
 UISet_C2S(PB(3), 'Tag', 'SelectMEScFile');
 
 generalTB(4) = GenericTextBox(MEScUIGroup);
-UISet_C2S(generalTB(4), 'Position', [0.012006861063465,0.550344827586227,0.974271012006861,0.143708145927016]);
+UISet_C2S(generalTB(4), 'Position', [0.18,0.75,0.974271012006861,0.143708145927016]);
 UISet_C2S(generalTB(4), 'String', 'No MESc file selected');
 UISet_C2S(generalTB(4), 'HorizontalAlignment', 'left');
 UISet_C2S(generalTB(4), 'ForeGroundColor', [0.4 0.4 0.4]);
 UISet_C2S(generalTB(4), 'BackGroundColor', [0.75 0.75 0.75]);
 
 MEScParamsUIGroup = uipanel('Title','MESc Params',...
-    'Position',[0.035,0.452380952380952,0.457,0.091005291005276],'BackgroundColor',[0.75 0.75 0.75]);
+    'FontSize', 10, ...
+    'Position',[0.035,0.412380952380952,0.457,0.13],...
+    'ForegroundColor',[0.3 0.3 0.3],...
+    'BackgroundColor',[0.75 0.75 0.75]);
+
+generalTB(13) = GenericTextBox(MEScParamsUIGroup);
+UISet_C2S(generalTB(13), 'Position', [0.012006861063465,0.550344827586227,0.974271012006861,0.3]);
+UISet_C2S(generalTB(13), 'String', 'FrameRate: ');
+UISet_C2S(generalTB(13), 'FontSize', 8);
+UISet_C2S(generalTB(13), 'HorizontalAlignment', 'left');
+UISet_C2S(generalTB(13), 'ForeGroundColor', [0.4 0.4 0.4]);
+UISet_C2S(generalTB(13), 'BackGroundColor', [0.75 0.75 0.75]);
+
+generalTB(14) = GenericTextBox(MEScParamsUIGroup);
+UISet_C2S(generalTB(14), 'Position', [0.012006861063465,0.060344827586227,0.974271012006861,0.3]);
+UISet_C2S(generalTB(14), 'String', 'TimeSteps in ms:');
+UISet_C2S(generalTB(14), 'FontSize', 8);
+UISet_C2S(generalTB(14), 'HorizontalAlignment', 'left');
+UISet_C2S(generalTB(14), 'ForeGroundColor', [0.4 0.4 0.4]);
+UISet_C2S(generalTB(14), 'BackGroundColor', [0.75 0.75 0.75]);
+
+generalTB(15) = GenericTextBox(MEScParamsUIGroup);
+UISet_C2S(generalTB(15), 'Position', [0.412006861063465,0.550344827586227,0.974271012006861,0.3]);
+UISet_C2S(generalTB(15), 'String', 'No. of planes detected:');
+UISet_C2S(generalTB(15), 'FontSize', 8);
+UISet_C2S(generalTB(15), 'HorizontalAlignment', 'left');
+UISet_C2S(generalTB(15), 'ForeGroundColor', [0.4 0.4 0.4]);
+UISet_C2S(generalTB(15), 'BackGroundColor', [0.75 0.75 0.75]);
 
 
-%%% - Analysis Console
-AnalysisUIGroup = uipanel('Title','Analysis',...
-    'Position',[0.52, 0.42, 0.45, 0.4],'BackgroundColor','white');
+%%% - 2. Analysis Console
+AnalysisUIGroup = uipanel('Title','Suite2p output management',...
+    'FontSize', 18, ...
+    'Position',[0.525, 0.38, 0.45, 0.45],...
+    'ForegroundColor',[0.5 0.5 0.5],...
+    'BackgroundColor','white');
 
-% - Specify which layer data is being analyzed
-DD_currentLayer = uicontrol(ExperimentFilesUIGroup);
+% - 2a. Layer selection spacecard
+LayerUIGroup = uipanel('Title','Layer selction',...
+    'FontSize', 15, ...
+    'Position',[0.53, 0.675, 0.44, 0.11],'BackgroundColor',[0.75 0.75 0.75]);
+
+% - 2a.i. Dropdown menu for layers
+DD_currentLayer = uicontrol(LayerUIGroup);
 UISet_C2S(DD_currentLayer,'Style','popupmenu')
 UISet_C2S(DD_currentLayer,'String', {'Select Layer', 'Layer 1', 'Layer 2','Layer 3', 'Layer 4','Layer 5'});
-UISet_C2S(DD_currentLayer, 'Position',[90,80,100,20]);
+UISet_C2S(DD_currentLayer, 'Position',[30,30,100,20]);
 UISet_C2S(DD_currentLayer, 'FontSize',10);
 UISet_C2S(DD_currentLayer, 'Enable', 'off');
 UISet_C2S(DD_currentLayer, 'Tag', 'LayerSelectionDD');
 UISet_C2S(DD_currentLayer, 'Callback', @callback_suite2pLayerSelection_C2S);
 
-% - Specify Fall.mat file selection button
-PB(4) = GenericPushButton(AnalysisUIGroup);
+generalTB(10) = GenericTextBox(LayerUIGroup);
+UISet_C2S(generalTB(10), 'Position', [0.02,0.05,0.974271012006861,0.3]);
+UISet_C2S(generalTB(10), 'String', 'Current layer: --');
+UISet_C2S(generalTB(10), 'HorizontalAlignment', 'left');
+UISet_C2S(generalTB(10), 'ForeGroundColor', [0.3 0.3 0.3]);
+UISet_C2S(generalTB(10), 'BackGroundColor', [0.75 0.75 0.75]);
+
+generalTB(11) = GenericTextBox(LayerUIGroup);
+UISet_C2S(generalTB(11), 'Position', [0.015,0.53,0.03,0.3]);
+UISet_C2S(generalTB(11), 'String',char(hex2dec('2713')));
+UISet_C2S(generalTB(11), 'HorizontalAlignment', 'left');
+UISet_C2S(generalTB(11), 'ForeGroundColor', [0.4 0.4 0.4]);
+UISet_C2S(generalTB(11), 'BackGroundColor', [0.75 0.75 0.75]);
+UISet_C2S(generalTB(11), 'FontSize', 12);
+UISet_C2S(generalTB(11), 'FontWeight', 'bold');
+
+generalTB(12) = GenericTextBox(LayerUIGroup);
+UISet_C2S(generalTB(12), 'Position', [0.32,0.05,0.8,0.3]);
+UISet_C2S(generalTB(12), 'String', 'No layers analyzed yet');
+UISet_C2S(generalTB(12), 'HorizontalAlignment', 'left');
+UISet_C2S(generalTB(12), 'ForeGroundColor', [0.3 0.3 0.3]);
+UISet_C2S(generalTB(12), 'BackGroundColor', [0.75 0.75 0.75]);
+
+
+% - 2b. Fall.mat file selection spacecard
+FallSelectionUIGroup = uipanel('Title','Suite2p Fall.mat file',...
+    'FontSize', 15, ...
+    'Position',[0.53,0.404,0.44,0.25375661375661],'BackgroundColor',[0.75 0.75 0.75]);
+
+PB(4) = GenericPushButton(FallSelectionUIGroup);
 UISet_C2S(PB(4), 'String', 'Browse');
-UISet_C2S(PB(4), 'Position', [0.05, 0.8, 0.12, 0.12]);
+UISet_C2S(PB(4), 'Position', [0.05, 0.75, 0.12, 0.18]);
 UISet_C2S(PB(4), 'Enable', 'off');
 UISet_C2S(PB(4), 'Callback', @callback_suite2pFallSelection_C2S);
 UISet_C2S(PB(4), 'Tag', 'FallSelection');
 
-generalTB(5) = GenericTextBox(AnalysisUIGroup);
+generalTB(5) = GenericTextBox(FallSelectionUIGroup);
 UISet_C2S(generalTB(5), 'Position', [0.2,0.745,0.974271012006861,0.143708145927016]);
-UISet_C2S(generalTB(5), 'String', 'Select Fall.mat File');
+UISet_C2S(generalTB(5), 'String', 'No file selected');
 UISet_C2S(generalTB(5), 'HorizontalAlignment', 'left');
 UISet_C2S(generalTB(5), 'ForeGroundColor', [0.3 0.3 0.3]);
+UISet_C2S(generalTB(5), 'BackGroundColor', [0.75 0.75 0.75]);
 
-generalTB(6) = GenericTextBox(AnalysisUIGroup);
-UISet_C2S(generalTB(6), 'Position', [0.015,0.82,0.025,0.07]);
+generalTB(6) = GenericTextBox(FallSelectionUIGroup);
+UISet_C2S(generalTB(6), 'Position', [0.015,0.74,0.03,0.15]);
 UISet_C2S(generalTB(6), 'String',char(hex2dec('2713')));
 UISet_C2S(generalTB(6), 'HorizontalAlignment', 'left');
 UISet_C2S(generalTB(6), 'ForeGroundColor', [0.4 0.4 0.4]);
-UISet_C2S(generalTB(6), 'BackGroundColor', 'w');
+UISet_C2S(generalTB(6), 'BackGroundColor', [0.75 0.75 0.75]);
 UISet_C2S(generalTB(6), 'FontSize', 12);
 UISet_C2S(generalTB(6), 'FontWeight', 'bold');
 
-generalTB(7) = GenericTextBox(AnalysisUIGroup);
-UISet_C2S(generalTB(7), 'Position', [0.015,0.82,0.025,0.07]);
-UISet_C2S(generalTB(7), 'String','No file selected')
-UISet_C2S(generalTB(7), 'HorizontalAlignment', 'left');
-UISet_C2S(generalTB(7), 'ForeGroundColor', [0.4 0.4 0.4]);
+
+FallParamsUIGroup = uipanel('Title','Fall.mat Params',...
+    'FontSize', 10, ...
+    'ForegroundColor',[0.3 0.3 0.3],...
+    'Position',[0.535,0.412380952380952,0.43,0.13],'BackgroundColor',[0.75 0.75 0.75]);
+
+generalTB(16) = GenericTextBox(FallParamsUIGroup);
+UISet_C2S(generalTB(16), 'Position', [0.01,0.75,0.974271012006861,0.143708145927016]);
+UISet_C2S(generalTB(16), 'String', 'No. of ROIs detected:');
+UISet_C2S(generalTB(16), 'FontSize', 8);
+UISet_C2S(generalTB(16), 'HorizontalAlignment', 'left');
+UISet_C2S(generalTB(16), 'ForeGroundColor', [0.4 0.4 0.4]);
+UISet_C2S(generalTB(16), 'BackGroundColor', [0.75 0.75 0.75]);
 
 
 
