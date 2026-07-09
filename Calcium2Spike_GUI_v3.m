094b0e0aad28c02e3fc19b6d8657839749d54a9a
ajax5692 <nn.abhra@gmail.com>
2026-07-09 12:07:03 +0200

    +feat: GUI updates
    added: restart GUI button and callback function, 'open folder' PB callback functions, analysis output space card

--- Calcium2Spike_GUI_v3.m
+++ Calcium2Spike_GUI_v3.m
@@ -12,7 +12,7 @@ end
 figureCalcium2Spike_GUI = figure;
 set(figureCalcium2Spike_GUI,'units', 'normalized',...
     'position', [0.1 0.1 0.7 0.7], 'Color', [0.6 0.6 0.6],...
-    'NumberTitle', 'off', 'Name', 'Calcium2Spike_GUI');
+    'NumberTitle', 'off', 'Name', 'Calcium2Spike_GUI','Resize','off');
     % 'MenuBar', 'none');
 
 
@@ -32,7 +32,7 @@ UISet_C2S(GUIheader, 'HorizontalAlignment','center');
 
 %error "console"
 errorConsole = GenericTextBox(figureCalcium2Spike_GUI);
-UISet_C2S(errorConsole, 'Position', [0.03, 0.03, 0.5, 0.05]);
+UISet_C2S(errorConsole, 'Position', [0.03, 0.03, 0.4, 0.05]);
 UISet_C2S(errorConsole, 'String', 'No errors');
 UISet_C2S(errorConsole, 'ForegroundColor', [0.64 0.08 0.18]);
 UISet_C2S(errorConsole, 'BackGroundColor', [0.85 0.85 0.85]);
@@ -44,7 +44,7 @@ guidata(figureCalcium2Spike_GUI, data);
 
 %logging "console"
 loggingConsole = GenericTextBox(figureCalcium2Spike_GUI);
-UISet_C2S(loggingConsole, 'Position', [0.55,0.03,0.42,0.047]);
+UISet_C2S(loggingConsole, 'Position', [0.45,0.03,0.4,0.047]);
 UISet_C2S(loggingConsole, 'String', data.logging.latestReturned);
 UISet_C2S(loggingConsole, 'ForegroundColor',[0.4667 0.6745 0.1882]);
 UISet_C2S(loggingConsole, 'BackGroundColor',[0.9 0.9 0.9]);
@@ -59,12 +59,19 @@ ExperimentFilesUIGroup = uipanel('Title','Project management',...
     'FontSize', 18,...
     'Position',[0.025, 0.38, 0.48, 0.45],...
     'ForegroundColor',[0.5, 0.5, 0.5],...
-    'BackgroundColor','White');
+    'BackgroundColor','White',...
+    'BorderType','line',...
+    'BorderColor','k',...
+    'ShadowColor','w');
 
 % - 1a. Save location spacecard
 SaveLocationUIGroup = uipanel('Title','Save location',...
     'FontSize', 15, ...
-    'Position',[0.030505952380952,0.673280423280423,0.468749999999999,0.11375661375661],'BackgroundColor',[0.75 0.75 0.75]);
+    'Position',[0.030505952380952,0.673280423280423,0.468749999999999,0.11375661375661],...
+    'BackgroundColor',[0.75 0.75 0.75],...
+    'BorderType','line',...
+    'BorderColor','k',...
+    'ShadowColor','w');
 
 generalTB(1) = GenericTextBox(SaveLocationUIGroup);
 UISet_C2S(generalTB(1), 'Position', [0.012006861063465,0.086956521739132,0.974271012006861,0.289855072463767]);
@@ -72,6 +79,8 @@ UISet_C2S(generalTB(1), 'String', 'No location specified');
 UISet_C2S(generalTB(1), 'HorizontalAlignment', 'left');
 UISet_C2S(generalTB(1), 'ForeGroundColor', [0.4 0.4 0.4]);
 UISet_C2S(generalTB(1), 'BackGroundColor', [0.75 0.75 0.75]);
+UISet_C2S(generalTB(1), 'Tag', 'SaveLocationPathTB');
+
 
 generalTB(2) = GenericTextBox(SaveLocationUIGroup);
 UISet_C2S(generalTB(2), 'Position', [0.01,0.56,0.974271012006861,0.289855072463767]);
@@ -81,10 +90,12 @@ UISet_C2S(generalTB(2), 'ForeGroundColor', [0.4 0.4 0.4]);
 UISet_C2S(generalTB(2), 'BackGroundColor', [0.75 0.75 0.75]);
 UISet_C2S(generalTB(2), 'FontSize', 12);
 UISet_C2S(generalTB(2), 'FontWeight', 'bold');
+UISet_C2S(generalTB(2), 'Tag', 'SaveLocationPathCheckmark');
 
 PB(1) = GenericPushButton(SaveLocationUIGroup);
 UISet_C2S(PB(1), 'String', 'Browse');
 UISet_C2S(PB(1), 'Position', [0.05,0.44,0.1,0.5]);
+UISet_C2S(PB(1), 'Tag', 'SaveLocationButton');
 UISet_C2S(PB(1), 'Callback', @callback_SpecifySaveLocation_C2S);
 
 PB(2) = GenericPushButton(SaveLocationUIGroup);
@@ -93,12 +104,18 @@ UISet_C2S(PB(2), 'FontSize', 7);
 UISet_C2S(PB(2), 'HorizontalAlignment', 'left');
 UISet_C2S(PB(2), 'Position', [0.881646655231561,0.057971014492756,0.110291595197254,0.260869565217391]);
 UISet_C2S(PB(2), 'TooltipString', ['Opens the folder containing the analyzed data.']);
-
+UISet_C2S(PB(2), 'Tag', 'OpenSaveLocationPathPB');
+UISet_C2S(PB(2), 'Enable', 'off');
+UISet_C2S(PB(2), 'Callback', @callback_OpenSaveLocationFolder_C2S);
 
 % - 1b. MESc spacecard
 MEScUIGroup = uipanel('Title','MESc file',...
     'FontSize', 15, ...
-    'Position',[0.030505952380952,0.402063492063488,0.468749999999999,0.255375661375661],'BackgroundColor',[0.75 0.75 0.75]);
+    'Position',[0.030505952380952,0.402063492063488,0.468749999999999,0.255375661375661],...
+    'BackgroundColor',[0.75 0.75 0.75],...
+    'BorderType','line',...
+    'BorderColor','k',...
+    'ShadowColor','w');
 
 generalTB(3) = GenericTextBox(MEScUIGroup);
 UISet_C2S(generalTB(3), 'Position', [0.01,0.63,0.974271012006861,0.289855072463767]);
@@ -108,6 +125,7 @@ UISet_C2S(generalTB(3), 'ForeGroundColor', [0.4 0.4 0.4]);
 UISet_C2S(generalTB(3), 'BackGroundColor', [0.75 0.75 0.75]);
 UISet_C2S(generalTB(3), 'FontSize', 12);
 UISet_C2S(generalTB(3), 'FontWeight', 'bold');
+UISet_C2S(generalTB(3), 'Tag', 'MEScFileSelectionCheckmark');
 
 PB(3) = GenericPushButton(MEScUIGroup);
 UISet_C2S(PB(3), 'String', 'Browse');
@@ -122,6 +140,7 @@ UISet_C2S(generalTB(4), 'String', 'No MESc file selected');
 UISet_C2S(generalTB(4), 'HorizontalAlignment', 'left');
 UISet_C2S(generalTB(4), 'ForeGroundColor', [0.4 0.4 0.4]);
 UISet_C2S(generalTB(4), 'BackGroundColor', [0.75 0.75 0.75]);
+UISet_C2S(generalTB(4), 'Tag', 'MEScFileLocationPathTB');
 
 MEScParamsUIGroup = uipanel('Title','MESc Params',...
     'FontSize', 10, ...
@@ -136,6 +155,7 @@ UISet_C2S(generalTB(5), 'FontSize', 8);
 UISet_C2S(generalTB(5), 'HorizontalAlignment', 'left');
 UISet_C2S(generalTB(5), 'ForeGroundColor', [0.4 0.4 0.4]);
 UISet_C2S(generalTB(5), 'BackGroundColor', [0.75 0.75 0.75]);
+UISet_C2S(generalTB(5), 'Tag', 'MEScFramerateTB');
 
 generalTB(6) = GenericTextBox(MEScParamsUIGroup);
 UISet_C2S(generalTB(6), 'Position', [0.012006861063465,0.060344827586227,0.974271012006861,0.3]);
@@ -144,6 +164,7 @@ UISet_C2S(generalTB(6), 'FontSize', 8);
 UISet_C2S(generalTB(6), 'HorizontalAlignment', 'left');
 UISet_C2S(generalTB(6), 'ForeGroundColor', [0.4 0.4 0.4]);
 UISet_C2S(generalTB(6), 'BackGroundColor', [0.75 0.75 0.75]);
+UISet_C2S(generalTB(6), 'Tag', 'MEScTimeStepsTB');
 
 generalTB(7) = GenericTextBox(MEScParamsUIGroup);
 UISet_C2S(generalTB(7), 'Position', [0.412006861063465,0.550344827586227,0.974271012006861,0.3]);
@@ -152,6 +173,7 @@ UISet_C2S(generalTB(7), 'FontSize', 8);
 UISet_C2S(generalTB(7), 'HorizontalAlignment', 'left');
 UISet_C2S(generalTB(7), 'ForeGroundColor', [0.4 0.4 0.4]);
 UISet_C2S(generalTB(7), 'BackGroundColor', [0.75 0.75 0.75]);
+UISet_C2S(generalTB(7), 'Tag', 'MEScMultiplaneTB');
 
 
 %%% - 2. Suite2p output management Console
@@ -159,12 +181,18 @@ Suite2pOutputUIGroup = uipanel('Title','Suite2p output management',...
     'FontSize', 18, ...
     'Position',[0.525, 0.38, 0.45, 0.45],...
     'ForegroundColor',[0.5 0.5 0.5],...
-    'BackgroundColor','white');
+    'BackgroundColor','white',...
+    'BorderType','line',...
+    'BorderColor','k',...
+    'ShadowColor','w');
 
 % - 2a. Layer selection spacecard
 LayerUIGroup = uipanel('Title','Layer selection',...
     'FontSize', 15, ...
-    'Position',[0.53, 0.675, 0.44, 0.11],'BackgroundColor',[0.75 0.75 0.75]);
+    'Position',[0.53, 0.67, 0.44, 0.115],'BackgroundColor',[0.75 0.75 0.75],...
+    'BorderType','line',...
+    'BorderColor','k',...
+    'ShadowColor','w');
 
 % - 2a.i. Dropdown menu for layers
 DD_currentLayer = uicontrol(LayerUIGroup);
@@ -182,6 +210,7 @@ UISet_C2S(generalTB(8), 'String', 'Current layer: --');
 UISet_C2S(generalTB(8), 'HorizontalAlignment', 'left');
 UISet_C2S(generalTB(8), 'ForeGroundColor', [0.3 0.3 0.3]);
 UISet_C2S(generalTB(8), 'BackGroundColor', [0.75 0.75 0.75]);
+UISet_C2S(generalTB(8), 'Tag', 'CurrentLayerTB');
 
 generalTB(9) = GenericTextBox(LayerUIGroup);
 UISet_C2S(generalTB(9), 'Position', [0.015,0.53,0.03,0.3]);
@@ -191,6 +220,7 @@ UISet_C2S(generalTB(9), 'ForeGroundColor', [0.4 0.4 0.4]);
 UISet_C2S(generalTB(9), 'BackGroundColor', [0.75 0.75 0.75]);
 UISet_C2S(generalTB(9), 'FontSize', 12);
 UISet_C2S(generalTB(9), 'FontWeight', 'bold');
+UISet_C2S(generalTB(9), 'Tag', 'LayerSelectionCheckmark');
 
 generalTB(10) = GenericTextBox(LayerUIGroup);
 UISet_C2S(generalTB(10), 'Position', [0.32,0.05,0.8,0.3]);
@@ -198,19 +228,23 @@ UISet_C2S(generalTB(10), 'String', 'No layers analyzed yet');
 UISet_C2S(generalTB(10), 'HorizontalAlignment', 'left');
 UISet_C2S(generalTB(10), 'ForeGroundColor', [0.3 0.3 0.3]);
 UISet_C2S(generalTB(10), 'BackGroundColor', [0.75 0.75 0.75]);
+UISet_C2S(generalTB(10), 'Tag', 'AnalyzedLayerTB');
 
 
 % - 2b. Fall.mat file selection spacecard
 FallSelectionUIGroup = uipanel('Title','Suite2p Fall.mat file',...
     'FontSize', 15, ...
-    'Position',[0.53,0.404,0.44,0.25375661375661],'BackgroundColor',[0.75 0.75 0.75]);
+    'Position',[0.53,0.404,0.44,0.25375661375661],'BackgroundColor',[0.75 0.75 0.75],...
+    'BorderType','line',...
+    'BorderColor','k',...
+    'ShadowColor','w');
 
 PB(4) = GenericPushButton(FallSelectionUIGroup);
 UISet_C2S(PB(4), 'String', 'Browse');
 UISet_C2S(PB(4), 'Position', [0.05, 0.75, 0.12, 0.18]);
 UISet_C2S(PB(4), 'Enable', 'off');
 UISet_C2S(PB(4), 'Callback', @callback_suite2pFallSelection_C2S);
-UISet_C2S(PB(4), 'Tag', 'FallSelection');
+UISet_C2S(PB(4), 'Tag', 'FallSelectionPB');
 
 generalTB(11) = GenericTextBox(FallSelectionUIGroup);
 UISet_C2S(generalTB(11), 'Position', [0.2,0.745,0.974271012006861,0.143708145927016]);
@@ -218,6 +252,7 @@ UISet_C2S(generalTB(11), 'String', 'No file selected');
 UISet_C2S(generalTB(11), 'HorizontalAlignment', 'left');
 UISet_C2S(generalTB(11), 'ForeGroundColor', [0.3 0.3 0.3]);
 UISet_C2S(generalTB(11), 'BackGroundColor', [0.75 0.75 0.75]);
+UISet_C2S(generalTB(11), 'Tag', 'FallFilepathTB');
 
 generalTB(12) = GenericTextBox(FallSelectionUIGroup);
 UISet_C2S(generalTB(12), 'Position', [0.015,0.74,0.03,0.15]);
@@ -227,7 +262,7 @@ UISet_C2S(generalTB(12), 'ForeGroundColor', [0.4 0.4 0.4]);
 UISet_C2S(generalTB(12), 'BackGroundColor', [0.75 0.75 0.75]);
 UISet_C2S(generalTB(12), 'FontSize', 12);
 UISet_C2S(generalTB(12), 'FontWeight', 'bold');
-
+UISet_C2S(generalTB(12), 'Tag', 'FallCheckmark');
 
 FallParamsUIGroup = uipanel('Title','Fall.mat Params',...
     'FontSize', 10, ...
@@ -241,37 +276,31 @@ UISet_C2S(generalTB(13), 'FontSize', 8);
 UISet_C2S(generalTB(13), 'HorizontalAlignment', 'left');
 UISet_C2S(generalTB(13), 'ForeGroundColor', [0.4 0.4 0.4]);
 UISet_C2S(generalTB(13), 'BackGroundColor', [0.75 0.75 0.75]);
-
+UISet_C2S(generalTB(13), 'Tag', 'FallROInumTB');
 
 %%% - 3. Analysis Console
 AnalysisUIGroup = uipanel('Title','Analysis',...
     'FontSize', 18, ...
     'Position',[0.025,0.1,0.95,0.255375661375661],...
     'ForegroundColor',[0.5 0.5 0.5],...
-    'BackgroundColor','white');
+    'BackgroundColor','white',...
+    'BorderType','line',...
+    'BorderColor','k',...
+    'ShadowColor','w');
 
 % - 3a. OASIS threshold editable textbox
 EB(1) = GenericEditBox(AnalysisUIGroup);
 UISet_C2S(EB(1), 'String', '0');
 UISet_C2S(EB(1), 'Position', [0.01,0.62,0.05,0.25]);
 UISet_C2S(EB(1), 'Enable', 'off');
-UISet_C2S(EB(1), 'Tag', 'SetOASISthreshold');
+UISet_C2S(EB(1), 'Tag', 'SetOASISthresholdEB');
 
 generalTB(14) = GenericTextBox(AnalysisUIGroup);
 UISet_C2S(generalTB(14), 'String', sprintf('Set OASIS threshold\n(default 0)'));
 UISet_C2S(generalTB(14), 'HorizontalAlignment', 'Left');
 UISet_C2S(generalTB(14), 'Position', [0.07,0.61,0.1,0.25]);
 UISet_C2S(generalTB(14), 'Enable', 'off');
-
-
-% - 3a. Run analysis button
-PB(5) = GenericPushButton(AnalysisUIGroup);
-UISet_C2S(PB(5), 'String', 'Run analysis');
-UISet_C2S(PB(5), 'Position', [0.25,0.62,0.1,0.25]);
-UISet_C2S(PB(5), 'Enable', 'off');
-UISet_C2S(PB(5), 'Tag', 'RunAnalysis');
-UISet_C2S(PB(5), 'Callback', @callback_CoreAnalysis_C2S);
-
+UISet_C2S(generalTB(14), 'Tag', 'SetOASISthresholdTB');
 
 % - 3b.i. Pool data checkbox
 CB(1) = GenericCheckBox(AnalysisUIGroup);
@@ -295,9 +324,26 @@ UISet_C2S(PB(6), 'FontWeight', 'normal');
 UISet_C2S(PB(6), 'Enable', 'off');
 UISet_C2S(PB(6), 'Callback', @callback_PoolData_C2S);
 
-% - 3c.i. Create the primary progress bar
-pbAxes1 = axes(AnalysisUIGroup,'Units', 'normalized', ...
-    'Position', [0.38 0.8 0.4 0.1], ...
+% - 3c. Core analysis space card
+CoreAnalysisUIGroup = uipanel('Title','',...
+    'FontSize', 10, ...
+    'ForegroundColor',[0.3 0.3 0.3],...
+    'Position',[0.25,0.13,0.718,0.19],'BackgroundColor',[0.75 0.75 0.75],...
+    'BorderType','line',...
+    'BorderColor',[0.75 0.75 0.75],...
+    'ShadowColor','w');
+
+% - 3c.i. Run analysis button
+PB(5) = GenericPushButton(CoreAnalysisUIGroup);
+UISet_C2S(PB(5), 'String', 'Run analysis');
+UISet_C2S(PB(5), 'Position', [0.015,0.62,0.1,0.25]);
+UISet_C2S(PB(5), 'Enable', 'off');
+UISet_C2S(PB(5), 'Tag', 'RunAnalysisPB');
+UISet_C2S(PB(5), 'Callback', @callback_CoreAnalysis_C2S);
+
+% - 3c.ii. Create the primary progress bar
+pbAxes1 = axes(CoreAnalysisUIGroup,'Units', 'normalized', ...
+    'Position', [0.18 0.8 0.55 0.1], ...
     'XLim', [0 1], ...
     'YLim', [0 1], ...
     'XTick', [], ...
@@ -313,12 +359,12 @@ pbPatch1 = patch( ...
 
 guidata(figureCalcium2Spike_GUI, data);
 
-% - 3c.ii. Primary progessbar "console"
-primaryPBconsole = GenericTextBox(AnalysisUIGroup);
-UISet_C2S(primaryPBconsole, 'Position', [0.79 0.785 0.1 0.12]);
+% - 3c.iii. Primary progessbar "console"
+primaryPBconsole = GenericTextBox(CoreAnalysisUIGroup);
+UISet_C2S(primaryPBconsole, 'Position', [0.74 0.79 0.1 0.12]);
 UISet_C2S(primaryPBconsole, 'String', 'Overall progress');
 UISet_C2S(primaryPBconsole, 'ForeGroundColor',[0 0 0]);
-UISet_C2S(primaryPBconsole, 'BackGroundColor','w');
+UISet_C2S(primaryPBconsole, 'BackGroundColor', [0.75 0.75 0.75]);
 UISet_C2S(primaryPBconsole, 'FontWeight','Bold');
 UISet_C2S(primaryPBconsole, 'HorizontalAlignment','Left');
 UISet_C2S(primaryPBconsole, 'Tag','primaryPBconsole');
@@ -326,9 +372,9 @@ UISet_C2S(primaryPBconsole, 'Tag','primaryPBconsole');
 data.GUI.primaryPBconsole = primaryPBconsole;
 guidata(figureCalcium2Spike_GUI, data);
 
-% - 3c.iii. Create the secondary progress bar
-pbAxes2 = axes(AnalysisUIGroup,'Units', 'normalized', ...
-    'Position', [0.38 0.6 0.4 0.1], ...
+% - 3c.iv. Create the secondary progress bar
+pbAxes2 = axes(CoreAnalysisUIGroup,'Units', 'normalized', ...
+    'Position', [0.18 0.6 0.55 0.1], ...
     'XLim', [0 1], ...
     'YLim', [0 1], ...
     'XTick', [], ...
@@ -344,12 +390,12 @@ pbPatch2 = patch( ...
 
 guidata(figureCalcium2Spike_GUI, data);
 
-% - 3c.iv. Secondary progessbar "console"
-secondaryPBconsole = GenericTextBox(AnalysisUIGroup);
-UISet_C2S(secondaryPBconsole, 'Position', [0.79 0.585 0.2 0.12]);
+% - 3c.v. Secondary progessbar "console"
+secondaryPBconsole = GenericTextBox(CoreAnalysisUIGroup);
+UISet_C2S(secondaryPBconsole, 'Position', [0.74 0.59 0.25 0.12]);
 UISet_C2S(secondaryPBconsole, 'String', 'Running Step: --');
 UISet_C2S(secondaryPBconsole, 'ForegroundColor',[0 0 0]);
-UISet_C2S(secondaryPBconsole, 'BackGroundColor','w');
+UISet_C2S(secondaryPBconsole, 'BackGroundColor', [0.75 0.75 0.75]);
 UISet_C2S(secondaryPBconsole, 'FontWeight','Bold');
 UISet_C2S(secondaryPBconsole, 'HorizontalAlignment','Left');
 UISet_C2S(secondaryPBconsole, 'Tag','secondaryPBconsole');
@@ -357,5 +403,47 @@ UISet_C2S(secondaryPBconsole, 'Tag','secondaryPBconsole');
 data.GUI.primaryPBconsole = primaryPBconsole;
 guidata(figureCalcium2Spike_GUI, data);
 
+% - 3c.vi. Analysis result space card"
+AnalysisResultUIGroup = uipanel('Title','Analysis Output',...
+    'FontSize', 10, ...
+    'ForegroundColor',[0.3 0.3 0.3],...
+    'Position',[0.26,0.14,0.705,0.09],'BackgroundColor',[0.75 0.75 0.75]);
+
+generalTB(15) = GenericTextBox(AnalysisResultUIGroup);
+UISet_C2S(generalTB(15), 'Position', [0.01,0.5,0.974271012006861,0.3]);
+UISet_C2S(generalTB(15), 'String', ('ΔF/F units (should match with no. of ROIs detected):'));
+UISet_C2S(generalTB(15), 'FontSize', 8);
+UISet_C2S(generalTB(15), 'HorizontalAlignment', 'left');
+UISet_C2S(generalTB(15), 'ForeGroundColor', [0.4 0.4 0.4]);
+UISet_C2S(generalTB(15), 'BackGroundColor', [0.75 0.75 0.75]);
+UISet_C2S(generalTB(15), 'Tag', 'DffCountTB');
+
+generalTB(16) = GenericTextBox(AnalysisResultUIGroup);
+UISet_C2S(generalTB(16), 'Position', [0.01,0.1,0.974271012006861,0.3]);
+UISet_C2S(generalTB(16), 'String', ('Units filtered due to low PSNR:'));
+UISet_C2S(generalTB(16), 'FontSize', 8);
+UISet_C2S(generalTB(16), 'HorizontalAlignment', 'left');
+UISet_C2S(generalTB(16), 'ForeGroundColor', [0.4 0.4 0.4]);
+UISet_C2S(generalTB(16), 'BackGroundColor', [0.75 0.75 0.75]);
+UISet_C2S(generalTB(16), 'Tag', 'PSNRfilterTB');
+
+generalTB(17) = GenericTextBox(AnalysisResultUIGroup);
+UISet_C2S(generalTB(17), 'Position', [0.41,0.5,0.974271012006861,0.3]);
+UISet_C2S(generalTB(17), 'String', ('Final ΔF/F units saved:'));
+UISet_C2S(generalTB(17), 'FontSize', 8);
+UISet_C2S(generalTB(17), 'HorizontalAlignment', 'left');
+UISet_C2S(generalTB(17), 'ForeGroundColor', [0.4 0.4 0.4]);
+UISet_C2S(generalTB(17), 'BackGroundColor', [0.75 0.75 0.75]);
+UISet_C2S(generalTB(17), 'Tag', 'DffSavedUnitsTB');
+
+% - 3c.i. Run analysis button
+PB(6) = GenericPushButton(figureCalcium2Spike_GUI);
+UISet_C2S(PB(6), 'String', 'Restart GUI');
+UISet_C2S(PB(6), 'Position', [0.895,0.028,0.08,0.055]);
+UISet_C2S(PB(6), 'Tag', 'RestartGuiPB');
+UISet_C2S(PB(6), 'Callback', @callback_resetCalcium2Spike_GUI_v3);
+
 % InformationPanel_C2S(figureCalcium2Spike_GUI);
 % ValuesHandle = DrawValues_C2S();
+
+aa=[];
--- callbacks/callback_SpecifySaveLocation_C2S.m
+++ callbacks/callback_SpecifySaveLocation_C2S.m
@@ -3,57 +3,127 @@ function callback_SpecifySaveLocation_C2S(hO, ed)
 figureCalcium2Spike_GUI = ancestor(hO,'figure');      % get figure that owns guidata
 d = guidata(hO);
 
-saveLocationTB = GenericTextBox(figureCalcium2Spike_GUI);
-UISet_C2S(saveLocationTB, 'Position', [0.025, 0.15, 0.945, 0.05]);
-UISet_C2S(saveLocationTB, 'BackGroundColor', [0.6 0.6 0.6]);
-UISet_C2S(saveLocationTB, 'HorizontalAlignment','left');
-UISet_C2S(saveLocationTB, 'Tag','saveLocationTB');
-
 saveDirectory = uigetdir('C:\Users\abhrajyoti.chakrabarti\Desktop\testNewGUI\','Please specify location where to save the results');
 
 try
     if saveDirectory ~= 0
         d.saveAnalyzedData = saveDirectory;
-        d = UpdateCheckmark_C2S(d,'savelocationspecified', 1);
         d = ToError(d, " No errors");
         UISet_C2S(d.GUI.errorConsole, 'String', d.errors.latestReturned,...
             'ForegroundColor',[0.64 0.08 0.18]);
         d = ToLog(d, "Save location successfully specified");
-        UISet_C2S(saveLocationTB, 'String', strcat('Data will be saved at:',{' '},d.saveAnalyzedData));
-        UISet_C2S(saveLocationTB, 'FontSize',8);
-        childrenArray = GUI_childrenFinder_C2S(d,'Layer-wise analysis','SelectMEScFile');
+        %update save location path text
+        childrenArray = GUI_childrenFinder_C2S(d,'Save location','SaveLocationPathTB');
+        UISet_C2S(d.source.Children(childrenArray(1)).Children(childrenArray(2)), 'String', ...
+            strcat('Data saved at:', {' '}, d.saveAnalyzedData));
+        %enable open save folder button
+        childrenArray = GUI_childrenFinder_C2S(d,'Save location','OpenSaveLocationPathPB');
         UISet_C2S(d.source.Children(childrenArray(1)).Children(childrenArray(2)), 'Enable', 'on');
+        %update checkmark color
+        childrenArray = GUI_childrenFinder_C2S(d,'Save location','SaveLocationPathCheckmark');
+        UISet_C2S(d.source.Children(childrenArray(1)).Children(childrenArray(2)), 'ForegroundColor',[0.21 0.70 0.21]);
+        %change directory and create folder to store analyzed data
         cd(d.saveAnalyzedData)
         mkdir('AnalyzedCalciumToSpikeData')
         cd(d.originalCodePath)
 
-    else
+    else %disable buttons and update GUI accordingly
         d = ToError(d, " Save location not specified by user");
-        d = UpdateCheckmark_C2S(d,'savelocationspecified', 0);
         UISet_C2S(d.GUI.errorConsole, 'String', d.errors.latestReturned,...
             'ForegroundColor',[0.64 0.08 0.18]);
-        UISet_C2S(saveLocationTB, 'String', 'Data will be saved at: --');
-        UISet_C2S(saveLocationTB, 'FontSize',8);
-        
-        childrenArray = GUI_childrenFinder_C2S(d,'Layer-wise analysis','SelectMEScFile');
+        % - savelocation space card
+        %reset save location path text
+        childrenArray = GUI_childrenFinder_C2S(d,'Save location','SaveLocationPathTB');
+        UISet_C2S(d.source.Children(childrenArray(1)).Children(childrenArray(2)), 'String', ...
+            'No location specified');
+        %disable open save folder PB
+        childrenArray = GUI_childrenFinder_C2S(d,'Save location','OpenSaveLocationPathPB');
         UISet_C2S(d.source.Children(childrenArray(1)).Children(childrenArray(2)), 'Enable', 'off');
-        d = UpdateCheckmark_C2S(d,'mescfileselected', 0);
-
-        childrenArray = GUI_childrenFinder_C2S(d,'Layer-wise analysis','LayerSelectionDD');
+        %update checkmark color
+        childrenArray = GUI_childrenFinder_C2S(d,'Save location','SaveLocationPathCheckmark');
+        UISet_C2S(d.source.Children(childrenArray(1)).Children(childrenArray(2)), 'ForegroundColor',[0.9 0.3 0.3]);
+        % - mesc space card
+        %disable MESc browse PB
+        childrenArray = GUI_childrenFinder_C2S(d,'MESc file','SelectMEScFile');
         UISet_C2S(d.source.Children(childrenArray(1)).Children(childrenArray(2)), 'Enable', 'off');
-        d = UpdateCheckmark_C2S(d,'layerselected', 0);
-
-        childrenArray = GUI_childrenFinder_C2S(d,'Layer-wise analysis','FallSelection');
-        UISet_C2S(d.source.Children(childrenArray(1)).Children(childrenArray(2)), 'Enable', 'off');
-        d = UpdateCheckmark_C2S(d,'fallfileselected', 0);
-
-        childrenArray = GUI_childrenFinder_C2S(d,'Layer-wise analysis','RunAnalysis');
-        UISet_C2S(d.source.Children(childrenArray(1)).Children(childrenArray(2)), 'Enable', 'off');
-        UISet_C2S(d.source.Children(childrenArray(1)).Children(childrenArray(2)), 'BackgroundColor', 'white');
-        UISet_C2S(d.source.Children(childrenArray(1)).Children(childrenArray(2)), 'FontWeight', 'normal');
-
-        childrenArray = GUI_childrenFinder_C2S(d,'','mescFileLocationTB')
-        UISet_C2S(d.source.Children(childrenArray), 'String', 'Selected MESc file: --');
+        %reset MESc filepath text
+        childrenArray = GUI_childrenFinder_C2S(d,'MESc file','MEScFileLocationPathTB');
+        UISet_C2S(d.source.Children(childrenArray(1)).Children(childrenArray(2)), 'String', ...
+            'No MESc file selected');
+        %update checkmark color
+        childrenArray = GUI_childrenFinder_C2S(d,'MESc file','MEScFileSelectionCheckmark');
+        UISet_C2S(d.source.Children(childrenArray(1)).Children(childrenArray(2)), 'ForegroundColor',[0.9 0.3 0.3]);
+        % - update MESc params space card
+        %framerate TB
+        childrenArray = GUI_childrenFinder_C2S(d,'MESc Params','MEScFramerateTB');
+        UISet_C2S(d.source.Children(childrenArray(1)).Children(childrenArray(2)), 'String', ...
+            'FrameRate:');
+        %timeSteps TB
+        childrenArray = GUI_childrenFinder_C2S(d,'MESc Params','MEScTimeStepsTB');
+        UISet_C2S(d.source.Children(childrenArray(1)).Children(childrenArray(2)), 'String', ...
+            'TimeSteps in ms:');
+        %no. of planes TB
+        childrenArray = GUI_childrenFinder_C2S(d,'MESc Params','MEScMultiplaneTB');
+        UISet_C2S(d.source.Children(childrenArray(1)).Children(childrenArray(2)), 'String', ...
+            'No. of planes detected:');
+        % - layer selection space card
+        %disable dropdown menu
+        childrenArray = GUI_childrenFinder_C2S(d,'Layer selection','LayerSelectionDD');
+        UISet_C2S(d.source.Children(childrenArray(1)).Children(childrenArray(2)), 'Enable','off');
+        %reset current layer TB
+        childrenArray = GUI_childrenFinder_C2S(d,'Layer selection','CurrentLayerTB');
+        UISet_C2S(d.source.Children(childrenArray(1)).Children(childrenArray(2)), 'String', ...
+            'Current layer: --');
+        %reset analyzed layer TB
+        childrenArray = GUI_childrenFinder_C2S(d,'Layer selection','AnalyzedLayerTB');
+        UISet_C2S(d.source.Children(childrenArray(1)).Children(childrenArray(2)), 'String', ...
+            'No layers analyzed yet');
+        %update checkmark color
+        childrenArray = GUI_childrenFinder_C2S(d,'Layer selection','LayerSelectionCheckmark');
+        UISet_C2S(d.source.Children(childrenArray(1)).Children(childrenArray(2)), 'ForegroundColor',[0.9 0.3 0.3]);
+        % - suite2p Fall space card
+        %disable pushbutton
+        childrenArray = GUI_childrenFinder_C2S(d,'Suite2p Fall.mat file','FallSelectionPB');
+        UISet_C2S(d.source.Children(childrenArray(1)).Children(childrenArray(2)), 'Enable','off');
+        %reset filepath TB
+        childrenArray = GUI_childrenFinder_C2S(d,'Suite2p Fall.mat file','FallFilepathTB');
+        UISet_C2S(d.source.Children(childrenArray(1)).Children(childrenArray(2)), 'String', ...
+            'No file selected');
+        %update checkmark color
+        childrenArray = GUI_childrenFinder_C2S(d,'Suite2p Fall.mat file','FallCheckmark');
+        UISet_C2S(d.source.Children(childrenArray(1)).Children(childrenArray(2)), 'ForegroundColor',[0.9 0.3 0.3]);
+        % - update Fall params space card
+        %no. of ROIs
+        childrenArray = GUI_childrenFinder_C2S(d,'Fall.mat Params','FallROInumTB');
+        UISet_C2S(d.source.Children(childrenArray(1)).Children(childrenArray(2)), 'String', ...
+            'No. of ROIs detected:');
+        % - update analysis space card
+        %disable OASIS EB
+        childrenArray = GUI_childrenFinder_C2S(d,'Analysis','SetOASISthresholdEB');
+        % UISet_C2S(d.source.Children(childrenArray(1)).Children(childrenArray(2)), 'String', ...
+        %     'No. of ROIs detected:');
+        %disable OASIS TB
+        childrenArray = GUI_childrenFinder_C2S(d,'Analysis','SetOASISthresholdTB');
+        UISet_C2S(d.source.Children(childrenArray(1)).Children(childrenArray(2)), 'Enable','off');
+        % - update core analysis space card
+        %disable run analysis PB
+        childrenArray = GUI_childrenFinder_C2S(d,'AnalysisResultUIGroup','RunAnalysisPB');
+        UISet_C2S(d.source.Children(childrenArray(1)).Children(childrenArray(2)), 'Enable', 'off',...
+            'BackgroundColor', 'w', 'ForegroundColor', 'k', 'FontWeight', 'normal', 'FontWeight',...
+            'normal', 'FontSize', 10);
+        % - reset the analysis output space card
+        %df/f count
+        childrenArray = GUI_childrenFinder_C2S(d,'AnalysisResultUIGroup','DffCountTB');
+        UISet_C2S(d.source.Children(childrenArray(1)).Children(childrenArray(2)), 'String', ...
+            'ΔF/F units (should match with no. of ROIs detected):');
+        %PSNR filtered units
+        childrenArray = GUI_childrenFinder_C2S(d,'AnalysisResultUIGroup','PSNRfilterTB');
+        UISet_C2S(d.source.Children(childrenArray(1)).Children(childrenArray(2)), 'String', ...
+            'Units filtered due to low PSNR:');
+        %saved df/f units
+        childrenArray = GUI_childrenFinder_C2S(d,'AnalysisResultUIGroup','DffSavedUnitsTB');
+        UISet_C2S(d.source.Children(childrenArray(1)).Children(childrenArray(2)), 'String', ...
+            'Final ΔF/F units saved:');
     end
 catch
 end
